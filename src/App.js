87e208f50bd5a37b5419e52dceaf8b2804bb067c
Fabrice HUET <fhuet99@gmail.com>
2021-09-25 00:15:00 +0200

    palette

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Test from './component/Test';
+import Palette from './component/Palette';
 
 function App() {
   return (
     <div >
       <h1>test</h1>
+      <Test/>
+      <Palette/>
     </div>
   );
 }
---
+++ src/component/Test.js
@@ -0,0 +1,19 @@
+import react from "react";
+import '../App.css';
+
+
+function Test() {
+
+    
+    return(
+        <>
+
+        <div>
+        <h2>Test</h2>            
+        </div>
+
+        </>
+    )
+}
+
+export default Test;
